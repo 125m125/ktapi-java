22c30322db908ca6052562d08452fb92eeb10b1e
125m125 <125m125@125m125.de>
2019-02-02 21:58:43 +0100

    fix checkstyle warnings in retrofit

--- ktapi-retrofit/src/test/java/de/_125m125/kt/ktapi/retrofit/Example.java
+++ ktapi-retrofit/src/test/java/de/_125m125/kt/ktapi/retrofit/Example.java
@@ -40,7 +40,8 @@ public class Example {
             }
 
             @Override
-            public void onFailure(final int status, final String message, final String humanReadableMessage) {
+            public void onFailure(final int status, final String message,
+                    final String humanReadableMessage) {
                 System.out.println("Request failed with status " + status + ": " + message);
             }
 
@@ -51,8 +52,9 @@ public class Example {
         });
 
         requester.ping().addCallback(Callback.successCallback(s -> r -> System.out.println(r)))
-        .addCallback(Callback.failureCallback(s -> s < 500 ? m -> h -> System.out.println("Client error: " + h)
-                : m -> h -> System.out.println("Server error: " + h)))
-        .addCallback(Callback.errorCallback(Throwable::printStackTrace));
+                .addCallback(Callback.failureCallback(
+                        s -> s < 500 ? m -> h -> System.out.println("Client error: " + h)
+                                : m -> h -> System.out.println("Server error: " + h)))
+                .addCallback(Callback.errorCallback(Throwable::printStackTrace));
     }
 }
